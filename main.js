abef9247994f33ae4236bb782cb28e3c81e50c2f
Leo-paul <leopaulmartin@hotmail.fr>
2021-05-30 23:54:03 +0200

    style: finalized

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-function clickItemNav() {
-    // TODO
+function openMenuBurger() {
+  console.log("openMenuBurger");
+  document.getElementById("menuBurger").style.display = "none";
+  document.getElementById("menuClose").style.display = "block";
+  var x = document.getElementsByClassName("navbar__menuGroup")[1];
+  x.classList.toggle("navbar__menuGroup--active");
+}
+function closeMenuBurger() {
+  console.log("closeMenuBurger");
+  document.getElementById("menuBurger").style.display = "block";
+  document.getElementById("menuClose").style.display = "none";
+  var x = document.getElementsByClassName("navbar__menuGroup")[1];
+  x.classList.toggle("navbar__menuGroup--active");
 }
